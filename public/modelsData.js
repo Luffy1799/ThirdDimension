740cb9c2457602485584c4aefeba4de009563c50
Luffy <35539292+Luffy1799@users.noreply.github.com>
2022-09-25 15:21:08 +0530

    Update modelsData.js

--- public/modelsData.js
+++ public/modelsData.js
@@ -2,84 +2,84 @@ export default [
     {   
         "id": '1001',
         "title": "Cheerios",
-        "path": "./models/p1.glb",
+        "path": "../models/p1.glb",
         "filename": "p1.glb"
     },
 
     {
         "id": '1002',
         "title": "Familia - Swiss Muesli",
-        "path": "./models/p2.glb",
+        "path": "../models/p2.glb",
         "filename": "p2.glb"
     },
 
     {
         "id": '1003',
         "title": "Pillsbury - Softasilk",
-        "path": "./models/p3.glb",
+        "path": "../models/p3.glb",
         "filename": "p3.glb"
     },
 
     {
         "id": '1004',
         "title": "Rice-a-Roni",
-        "path": "./models/p4.glb",
+        "path": "../models/p4.glb",
         "filename": "p4.glb"
     },
 
     {   
         "id": '1005',
         "title": "Raley's Farfalle",
-        "path": "./models/p6.glb",
+        "path": "../models/p6.glb",
         "filename": "p6.glb"
     },
 
     {   
         "id": '1006',
         "title": "Minule - Brown rice and quinoa",
-        "path": "./models/p7.glb",
+        "path": "../models/p7.glb",
         "filename": "p7.glb"
     },
 
     {   
         "id": '1007',
         "title": "Nature Valley",
-        "path": "./models/p8.glb",
+        "path": "../models/p8.glb",
         "filename": "p8.glb"
     },
 
     {   
         "id": '1008',
         "title": "Bigelow - Earl Grey",
-        "path": "./models/p9.glb",
+        "path": "../models/p9.glb",
         "filename": "p9.glb"
     },
 
     {   
         "id": '1009',
         "title": "Special K",
-        "path": "./models/p12.glb",
+        "path": "../models/p12.glb",
         "filename": "p12.glb"
     },
 
     {   
         "id": '1010',
         "title": "Kellogg's Muesli Fruit Magic",
-        "path": "./models/p13.glb",
+        "path": "../models/p13.glb",
         "filename": "p13.glb"
     },
 
     {   
         "id": '1011',
         "title": "Kellogg's - Real Honey",
-        "path": "./models/p17.glb",
+        "path": "../models/p17.glb",
         "filename": "p17.glb"
     },
 
     {   
         "id": '1012',
         "title": "Kellogg's Muesli Nuts Delight",
-        "path": "./models/p18.glb",
+        "path": "../models/p18.glb",
         "filename": "p18.glb"
         
     }
